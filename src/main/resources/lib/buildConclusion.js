f8587deac443e4ace97990e33ef1a06af9c84626
Justin Searls <searls@gmail.com>
2011-03-02 10:45:55 -0500

    Conclusion now counts the total of "not passed", but the total is only of the results. will need to kill this file

--- src/main/resources/lib/buildConclusion.js
+++ src/main/resources/lib/buildConclusion.js
@@ -2,7 +2,7 @@ if(typeof reporter !== 'undefined') {
 	var specCount = 0; 
 	var failCount=0; 
 	for(var key in reporter.results()) { 
-		specCount++; if(reporter.results()[key].result == 'failed') failCount++; 
+		specCount++; if(reporter.results()[key].result !== 'passed') failCount++; 
 	} 
 	specCount+' specs, '+failCount+' failures';
 }
