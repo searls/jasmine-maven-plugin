cbe6e120e3bd1188e03602fe3337057eb952ff67
Justin Searls <searls@gmail.com>
2011-07-04 12:30:15 -0400

    more docs

--- js/jasmine-maven-plugin.js
+++ js/jasmine-maven-plugin.js
@@ -1,7 +1,7 @@
 (function($) {
 
-
-  $(function() {
-    
+  $('.show-more').live('click',function(e) {
+    e.preventDefault();
+    $(this).closest('.expandable').find('.more').toggle('blind');
   });
 })(jQuery);
