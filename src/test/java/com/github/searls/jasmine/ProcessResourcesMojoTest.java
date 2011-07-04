8767e1887c05235487e9306e069c7354e0e63acf
Justin Searls <searls@gmail.com>
2011-07-03 21:57:14 -0400

    added coffee to the resource mojo

--- src/test/java/com/github/searls/jasmine/ProcessResourcesMojoTest.java
+++ src/test/java/com/github/searls/jasmine/ProcessResourcesMojoTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.*;
 import java.io.File;
 import java.io.IOException;
 
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
@@ -14,35 +15,87 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.github.searls.jasmine.coffee.CompilesAllCoffeeInDirectory;
 import com.github.searls.jasmine.io.DirectoryCopier;
 import com.github.searls.jasmine.model.ScriptSearch;
 
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ProcessResourcesMojo.class)
 public class ProcessResourcesMojoTest {
 
-	@InjectMocks private ProcessTestResourcesMojo subject = new ProcessTestResourcesMojo();
+	private static final String SRC_DIR_NAME = "blarh";
+	
+	@InjectMocks private ProcessResourcesMojo subject = new ProcessResourcesMojo();
+	
 	@Mock private DirectoryCopier directoryCopier;
+	@Mock private CompilesAllCoffeeInDirectory compilesAllCoffeeInDirectory;
+	
+	@Mock private File jasmineTargetDir;
+	
+	@Mock private File sourceDir;
+	@Mock private File targetDir;
+	@Mock private ScriptSearch sources;
+	
+	@Mock private Log log;
 	
 	@Before
 	public void killLogging() {
-		subject.setLog(mock(Log.class));
+		subject.setLog(log);
+	}
+	
+	@Before
+	public void isolateSubject() throws Exception {
+		subject.jasmineTargetDir = jasmineTargetDir;
+		subject.srcDirectoryName = SRC_DIR_NAME;
+		subject.sources = sources;
+
+		whenNew(File.class).withArguments(jasmineTargetDir,SRC_DIR_NAME).thenReturn(targetDir);
+	}
+	
+	@Before
+	public void stubSources() {
+		when(sources.getDirectory()).thenReturn(sourceDir);
 	}
 	
 	@Test
-	public void shouldUseDirectoryCopier() throws IOException, MojoExecutionException, MojoFailureException {
-		File srcDir = mock(File.class);
-		when(srcDir.exists()).thenReturn(true);
-		subject.specs = new ScriptSearch(srcDir,null,null);
-		subject.specDirectoryName = "anything";
+	public void basicLogging() throws IOException {
+		subject.run();
+		
+		verify(log).info("Processing JavaScript Sources");
+	}
+
+	@Test
+	public void whenDirectoryExistsCopy() throws IOException, MojoExecutionException, MojoFailureException {
+		when(sourceDir.exists()).thenReturn(true);
 		
 		subject.run();
 		
-		verify(directoryCopier).copyDirectory(eq(srcDir), isA(File.class));
+		verify(directoryCopier).copyDirectory(sources.getDirectory(), targetDir);
 	}
 	
+	@Test
+	public void whenDirectoryExistsCoffeeTime() throws IOException {
+		when(sourceDir.exists()).thenReturn(true);
+		
+		subject.run();
+		
+		verify(compilesAllCoffeeInDirectory).compile(targetDir);
+	}
 	
+	@Test
+	public void whenDirectoryDoesNotExistDoNotDoStuff() throws IOException, MojoExecutionException, MojoFailureException {
+		when(sources.getDirectory()).thenReturn(sourceDir);
+		when(sourceDir.exists()).thenReturn(false);
+		
+		subject.run();
+		
+		verify(directoryCopier,never()).copyDirectory(any(File.class),any(File.class));
+		verify(compilesAllCoffeeInDirectory,never()).compile(any(File.class));
+		verify(log).warn(ProcessResourcesMojo.MISSING_DIR_WARNING);
+	}
 	
 }
