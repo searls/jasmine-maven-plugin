89ffc18961d85cdf523a3f58150764e8cef8502d
johnomalley <john.c.omalley@gmail.com>
2013-02-15 10:41:15 -0600

    Reverted to previous versions in preparation for adding phantomJS support.

--- src/main/java/com/github/searls/jasmine/TestMojo.java
+++ src/main/java/com/github/searls/jasmine/TestMojo.java
@@ -1,5 +1,19 @@
 package com.github.searls.jasmine;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.net.MalformedURLException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoFailureException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.IncorrectnessListener;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
 import com.github.searls.jasmine.format.JasmineResultLogger;
 import com.github.searls.jasmine.io.scripts.TargetDirScriptResolver;
 import com.github.searls.jasmine.model.JasmineResult;
@@ -7,13 +21,7 @@ import com.github.searls.jasmine.runner.ReporterType;
 import com.github.searls.jasmine.runner.SpecRunnerExecutor;
 import com.github.searls.jasmine.runner.SpecRunnerHtmlGenerator;
 import com.github.searls.jasmine.runner.SpecRunnerHtmlGeneratorFactory;
-import org.apache.commons.io.FileUtils;
-import org.apache.maven.plugin.MojoFailureException;
-import org.openqa.selenium.WebDriver;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
 
 /**
  * @component
@@ -58,8 +66,41 @@ public class TestMojo extends AbstractJasmineMojo {
   }
 
   private WebDriver createDriver() {
-    return new WebDriverConfiguration(webDriverClassName, remoteWebDriverUrl, browserVersion, debug).createWebDriver();
+    if (!HtmlUnitDriver.class.getName().equals(webDriverClassName)) {
+      try {
+        @SuppressWarnings("unchecked")
+        Class<? extends WebDriver> klass = (Class<? extends WebDriver>) Class.forName(webDriverClassName);
+        Constructor<? extends WebDriver> ctor = klass.getConstructor();
+        return ctor.newInstance();
+      } catch (Exception e) {
+        throw new RuntimeException("Couldn't instantiate webDriverClassName", e);
+      }
+    }
+
+    // We have extra configuration to do to the HtmlUnitDriver
+    BrowserVersion htmlUnitBrowserVersion;
+    try {
+      htmlUnitBrowserVersion = (BrowserVersion) BrowserVersion.class.getField(browserVersion).get(BrowserVersion.class);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    HtmlUnitDriver driver = new HtmlUnitDriver(htmlUnitBrowserVersion) {
+      protected WebClient modifyWebClient(WebClient client) {
+        client.setAjaxController(new NicelyResynchronizingAjaxController());
+
+        //Disables stuff like this "com.gargoylesoftware.htmlunit.IncorrectnessListenerImpl notify WARNING: Obsolete content type encountered: 'text/javascript'."
+        if (!debug)
+          client.setIncorrectnessListener(new IncorrectnessListener() {
+                public void notify(String arg0, Object arg1) {}
+        });
+
+        return client;
+      };
+    };
+    driver.setJavascriptEnabled(true);
+    return driver;
   }
+
 
   private void logResults(JasmineResult result) {
     JasmineResultLogger resultLogger = new JasmineResultLogger();
