2b52de73583c95cf64d4e55edefe4e9ee3f67715
Chris Henzie <chrishenzie@gmail.com>
2014-01-28 18:54:34 -0800

    Added support for list and boolean values in capabilities map so you can use <arg>, <detach>, etc.

--- src/main/java/com/github/searls/jasmine/mojo/Capability.java
+++ src/main/java/com/github/searls/jasmine/mojo/Capability.java
@@ -1,5 +1,6 @@
 package com.github.searls.jasmine.mojo;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -8,7 +9,7 @@ public class Capability {
   private String name;
   private String value;
   private List<String> list;
-  private Map<String,String> map;
+  private Map<String,Object> map;
 
   public String getName() {
     return name;
@@ -34,11 +35,29 @@ public class Capability {
     this.list = list;
   }
 
-  public Map<String, String> getMap() {
+  public Map<String, Object> getMap() {
     return map;
   }
 
-  public void setMap(Map<String, String> map) {
+  public void setMap(Map<String, Object> map) {
+    for (String key : map.keySet()) {
+      String valueStr = (String) map.get(key);
+      if (isListString(valueStr)) {
+        List<String> listValue = Arrays.asList(valueStr.substring(1, valueStr.length() - 1).split(","));
+        map.put(key, listValue);
+      } else if (isBooleanString(valueStr)) {
+        Boolean booleanValue = Boolean.parseBoolean(valueStr);
+        map.put(key, booleanValue);
+      }
+    }
     this.map = map;
+  }
+  
+  private boolean isListString(String str) {
+      return str.charAt(0) == '[' && str.charAt(str.length() - 1) == ']';
+  }
+
+  private boolean isBooleanString(String str) {
+    return "true".equals(str) || "false".equals(str);
   }
 }
