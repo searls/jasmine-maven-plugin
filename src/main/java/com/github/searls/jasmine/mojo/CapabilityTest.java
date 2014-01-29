5f31514a060100b9457013874f69646d682334dc
Chris Henzie <chrishenzie@gmail.com>
2014-01-28 22:35:50 -0800

    Adds test coverage for new setMap() method

---
+++ src/main/java/com/github/searls/jasmine/mojo/CapabilityTest.java
@@ -0,0 +1,31 @@
+package com.github.searls.jasmine.mojo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
+
+public class CapabilityTest {
+
+    @Test
+    public void setMapTest() {
+        Map<String, Object> testMap = new HashMap<String, Object>();
+        testMap.put("detach", "true");
+        testMap.put("args", "[crash-test,no-sandbox]");
+
+        Capability capability = new Capability();
+        capability.setMap(testMap);
+        Map<String, Object> result = capability.getMap();
+        assertTrue((Boolean) result.get("detach"));
+
+        List<String> argsList = Arrays.asList(new String[]{"crash-test", "no-sandbox"});
+        assertEquals(argsList, result.get("args"));
+    }
+
+}
