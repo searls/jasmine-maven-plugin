d7012c8d3b0c3134ce663f6113c3a373bf430533
Øystein Kjærnet <kjarnet@gmail.com>
2015-03-05 15:06:14 +0100

    Updated to jasmine 2.1.3,
    and updated require.js to 2.1.16 in the amd-tests.

--- src/test/resources/examples/jasmine-webapp-amd-support/src/test/javascript/lib/jasmine.fooMatcher.js
+++ src/test/resources/examples/jasmine-webapp-amd-support/src/test/javascript/lib/jasmine.fooMatcher.js
@@ -1,14 +1,16 @@
-jasmine.addMatchers({
-  toEqualFoo : function(util, customEqualityTesters) {
+define([], function() {
     return {
-      compare: function(actual, expected) {
-        var rx = new RegExp("Foo" + expected);
-        var passed = rx.test(actual);
-        return {
-          pass: passed,
-          message: 'Expected ' + actual + (passed ? '' : ' not') + ' to match ' + expected
-        };
-      }
+        toEqualFoo : function(util, customEqualityTesters) {
+            return {
+                compare: function(actual, expected) {
+                    var rx = new RegExp("Foo" + expected);
+                    var passed = rx.test(actual);
+                    return {
+                        pass: passed,
+                        message: 'Expected ' + actual + (passed ? '' : ' not') + ' to match ' + expected
+                    };
+                }
+            };
+        }
     };
-  }
 });
