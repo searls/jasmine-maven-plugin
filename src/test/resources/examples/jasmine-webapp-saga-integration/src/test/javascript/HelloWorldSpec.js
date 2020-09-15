0b121f71d9f4f0452387ce7f1b912d7dccce1cb4
Kyle Lieber <klieber@users.noreply.github.com>
2020-09-15 13:58:16 -0500

    fix: integration with saga-maven-plugin no longer supported

--- src/test/resources/examples/jasmine-webapp-saga-integration/src/test/javascript/HelloWorldSpec.js
+++
@@ -1,8 +0,0 @@
-describe('HelloWorld',function(){
-
-  it('should say hello',function(){
-    var helloWorld = new HelloWorld();
-    expect(helloWorld.greeting()).toBe("Hello, World");
-  });
-
-});
